6a2e758dec6ae1fda27917b13423be329c864020
Keddar Noreddine <noreddinekeddar@gmail.com>
2022-02-27 23:18:24 +0100

    edition cocktail

--- src/app/cocktail-container/cocktail-form/cocktail-form.component.ts
+++ src/app/cocktail-container/cocktail-form/cocktail-form.component.ts
@@ -1,40 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { CocktailService } from 'src/app/shared/interfaces/services/cocktail.service';
 
 @Component({
   selector: 'app-cocktail-form',
   templateUrl: './cocktail-form.component.html',
-  styleUrls: ['./cocktail-form.component.scss']
+  styleUrls: ['./cocktail-form.component.scss'],
 })
 export class CocktailFormComponent implements OnInit {
-  public cocktailForm! : FormGroup;
-  constructor(private fb : FormBuilder, private cocktailservice : CocktailService, private router : Router, private activatedRoute : ActivatedRoute) { }
+  public cocktail: Cockta;
+  public cocktailForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    img: ['', Validators.required],
+    description: '',
+    ingredients: this.fb.array([], Validators.required),
+  });
 
-  ngOnInit(): void {
-    this.cocktailForm = this.fb.group({
-      name : ['', Validators.required],
-      img : ['', Validators.required],
-      description : ['', Validators.required],
-      ingredients : this.fb.array([Validators.required]),
-    })
-  }
-
-  public get ingredients(){
+  public get ingredients() {
     return this.cocktailForm.get('ingredients') as FormArray;
   }
-  addIngredient() : void {
-    this.ingredients.push(this.fb.group({
-      name : ['',Validators.required],
-      quantity : [0, Validators.required]
-    }))
 
+  constructor(
+    private fb: FormBuilder,
+    private cocktailService: CocktailService,
+    public router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.cocktail = this.cocktailService.getCocktail(index);
+    });
   }
 
-  submit():void{
-    this.cocktailservice.addCocktail(this.cocktailForm.value)
-    this.router.navigate(['..'], {relativeTo : this.activatedRoute})
+  public addIngredient(): void {
+    this.ingredients.push(
+      this.fb.group({
+        name: ['', Validators.required],
+        quantity: [0, Validators.required],
+      })
+    );
   }
 
+  public submit(): void {
+    this.cocktailService.addCocktail(this.cocktailForm.value);
+    this.router.navigate(['..'], { relativeTo: this.activatedRoute });
+  }
 }
